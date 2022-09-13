99dd2998d3cec7890fdc65148417db7d26f3793b
Dimitri Prima Nugraha <100235379+dimitripn@users.noreply.github.com>
2022-09-13 08:11:26 +0700

    Update views.py

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # TODO: Create your views here.
+from katalog.models import CatalogItem
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog': data_katalog,
+        'name': 'Dimitri Prima Nugraha',
+        'id': '2106750396',
+    }
+    return render(request, "katalog.html", context)
